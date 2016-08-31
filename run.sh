824bec8ffe11e51c9d71c9441df9cf209ca7b3ff
mich83 <mich83@yandex.ru>
2016-08-31 17:32:36 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /tmp
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
-tar xfj phantomjs-2.1.1-linux-x86_64.tar.bz2
-sudo cp /tmp/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin
+wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/phantomjs/phantomjs-1.9.1-linux-x86_64.tar.bz2
+tar xfj phantomjs-1.9.1-linux-x86_64.tar.bz2
+sudo cp /tmp/phantomjs-1.9.1-linux-x86_64/bin/phantomjs /usr/local/bin
